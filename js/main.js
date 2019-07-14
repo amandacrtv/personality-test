821d590728cf7e9a76faf2eb4b52d43be18450b2
Amanda Tavares <amandacrisrodriguest@gmail.com>
2019-07-14 11:25:08 -0300

    Update: change code syntax

--- js/main.js
+++ js/main.js
@@ -1,48 +1,49 @@
 
-//Toxiclibs Polygon2D class helper
-const Polygon2D = toxi.geom.Polygon2D;
+const MAIN = (SKETCH) => {
 
-//floor square width
-const tile_w = 77/2;
+	//floor square width
+	const tile_w = 77/2;
 
-//floor square height
-const tile_h = 38;
+	//floor square height
+	const tile_h = 38;
 
-//complete floor height
-const floor_h = 50;
+	//complete floor height
+	const floor_h = 50;
 
-//position
-let x, y, offsetX, offsetY;
-let p = createVector(0, 0);
+	//position
+	let x, y, offsetX, offsetY;
+	let p = SKETCH.createVector(0, 0);
 
-//font
-let font;
+	//font
+	let font;
 
-function setup() {
-  createCanvas(720, 400);
-  // Pick colors randomly
-  r = random(255);
-  g = random(255);
-  b = random(255);
-}
+	SKETCH.setup = () => {
+		SKETCH.createCanvas(720, 400);
+		// Pick colors randomly
+		r = SKETCH.random(255);
+		g = SKETCH.random(255);
+		b = SKETCH.random(255);
+	};
 
-function draw() {
-  background(127);
-  // Draw a circle
-  strokeWeight(2);
-  stroke(r, g, b);
-  fill(r, g, b, 127);
-  ellipse(360, 200, 200, 200);
-}
+	SKETCH.draw = () => {
+		SKETCH.background(127);
+		// Draw a circle
+		SKETCH.strokeWeight(2);
+		SKETCH.stroke(r, g, b);
+		SKETCH.fill(r, g, b, 127);
+		SKETCH.ellipse(360, 200, 200, 200);
+	};
 
-// When the user clicks the mouse
-function mousePressed() {
-  // Check if mouse is inside the circle
-  let d = dist(mouseX, mouseY, 360, 200);
-  if (d < 100) {
-    // Pick new random color values
-    r = random(255);
-    g = random(255);
-    b = random(255);
-  }
-}
+	SKETCH.mousePressed = () => {
+		// Check if mouse is inside the circle
+		let d = SKETCH.dist(SKETCH.mouseX, SKETCH.mouseY, 360, 200);
+		if (d < 100) {
+			// Pick new random color values
+			r = SKETCH.random(255);
+			g = SKETCH.random(255);
+			b = SKETCH.random(255);
+		}
+	};
+};
+
+const P5 = new p5(MAIN);
