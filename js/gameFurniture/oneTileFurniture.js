f0f918d2a52b760190f9a1c3750009b07551ed6f
Amanda Cristina <amandacrisrodriguest@gmail.com>
2019-07-16 12:16:18 -0300

    .

--- js/gameFurniture/oneTileFurniture.js
+++ js/gameFurniture/oneTileFurniture.js
@@ -1,12 +1,14 @@
 //furniture that occupies only one tile with N rotation states
 class oneTileFurniture extends gameFurniture {
-    constructor (s, personality, n, ns, score) {
+    constructor(s, personality, n, ns, score) {
         super(s, personality, n, score, 1);
         this.states.tileStates = new Array();
         this.states.stateImage = new Array();
         for (i = 0; i < ns; i++) {
             this.states.tileStates[i] = createVector(1, 1);
-            this.states.stateImage[i] = loadImage(s, this.defineState(i) + '.png');
+            this.states.stateImage[i] = loadImage(
+                s + this.defineState(i) + '.png'
+            );
         }
     }
 }
--- js/gameFurniture/twoTileFurniture.js
+++ js/gameFurniture/twoTileFurniture.js
@@ -1,13 +1,15 @@
 //furniture that only has two rotation states
 class twoTileFurniture extends gameFurniture {
-    constructor (s, personality, n) {
+    constructor(s, personality, n) {
         super(s, personality, n, 0.5, 4);
         this.states.tileStates = new Array();
         this.states.stateImage = new Array();
         for (i = 0; i < 2; i++) {
             if (i == 0) states.tileStates[i] = createVector(2, 1);
             else this.states.tileStates[i] = createVector(1, 2);
-            this.states.stateImage[i] = loadImage(s, this.defineState(i) + '.png');
+            this.states.stateImage[i] = loadImage(
+                s + this.defineState(i) + '.png'
+            );
         }
     }
 }
